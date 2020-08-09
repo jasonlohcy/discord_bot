9ece2668faab2b543aaeabf6caba7ab6e3425c9a
Jason Loh <jasonlohcy96@gmail.com>
2020-08-09 19:11:07 +0800

    fixed help again

--- bot.py
+++ bot.py
@@ -15,6 +15,13 @@ emoji_dict = {
     'poggerschain' : '<a:aPES_PoggersChain:619920297750953989>',
     'pepescary' : '<a:PepeScary:731173526102605885>',
     'pogspin' : '<a:aPES_PogSpin:699598474143989830>',
+    'mochiheart' : '<a:0MochiHeart:641898366996971520>',
+    'peperain' : '<a:aPES_PeepoSadRain:718071523864477726>',
+    'ezmoney' : '<a:aPES_MoneyRain:718071523524739074>',
+    'peplaugh' : '<a:aPES_LulLaugh:718071523013034125>',
+    'monkawash' : '<a:aPES_FranticWash:690958321733337170>',
+    'pepebonk' : '<a:aPES4_PaperBonk:733230501455986769>',
+    'kittyplay' : '<a:0KittyPlay:700970912656392204>'
 }
 
 webhook_dict = {
@@ -26,10 +33,11 @@ webhook_dict = {
 client = discord.Client()
 
 def create_help(emoji_dict):
-    embed = discord.Embed(title='**Emoji Help**')
+    embed = discord.Embed(title='**Emoji Help**',desciption='[Github Source Code](https://github.com/jasonlohcy/discord_bot)')
     for key, value in emoji_dict.items():
         #emoji , command , inline=true
         embed.add_field(name=value,value=f'`{PREFIX}{key}`')
+    
     return embed
 
 def send_emoji(emoji, message):
