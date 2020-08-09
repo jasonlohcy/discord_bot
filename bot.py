4c8c5cc01e8c34b1a774a334d36234df953af9a9
Jason Loh <jasonlohcy96@gmail.com>
2020-08-09 16:55:51 +0800

    first commit

---
+++ bot.py
@@ -0,0 +1,73 @@
+import discord
+from discord import Webhook, RequestsWebhookAdapter
+import os
+
+TOKEN = os.environ['TOKEN']  
+PREFIX = '.'
+
+emoji_dict = {
+    'shoot_tnt' : '<a:KaguyaPew_01s:697513110759800843> <a:KaguyaPew_02s:697513110206152744> '\
+                    '<a:KaguyaPew_03s:697513110709469274> <a:KaguyaPew_04s:697513110118072422> <a:TNT_ignite:697513136357376002>',
+    'grass_bounce' : '<a:grass_bounce:587505418406723584>',
+    'fast_parrot' : '<a:fast_parrot:393622342581878785>'
+}
+
+webhook_dict = {
+    '🤫-secret' : os.environ['SECRET_CHANNEL'],
+    '🏛-hidden-link' : os.environ['HIDDEN_LINK'],
+    'minecraft-progression' : os.environ['MINECRAFT_PROGRESSION'],
+}
+
+client = discord.Client()
+
+def create_help(emoji_dict):
+    embed = discord.Embed(title='**Emoji Help**')
+    for key, value in emoji_dict.items():
+        #debug
+        print(key, value)
+        #emoji , command , inline=true
+        embed.add_field(name=value,value=f'`{PREFIX}{key}`')
+    return embed
+
+def send_emoji(emoji, message):
+    emote = emoji_dict[emoji]
+    if not message.channel in webhook_dict:
+        print(f'{message.channel}channel not supported')
+        return
+    webhook = Webhook.from_url(webhook_dict[message.channel],adapter=RequestsWebhookAdapter())
+    webhook.send(content=emote,username=message.author.display_name, avatar_url=message.author.avatar_url)
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    #could use discord.py ext command
+    if not message.content.startswith(PREFIX):
+        return
+
+    if message.content.startswith(PREFIX + 'test'):
+        print('author.name {0.author.name}, author.avatar_url {0.author.avatar_url}'.format(message))
+        await message.channel.send('Hello!')
+        c = '{0.channel}'.format(message)
+        print(c)
+    
+    if message.content.startswith(PREFIX + 'help'):
+        embed = create_help(emoji_dict)
+        webhook = Webhook.from_url(webhook_url,adapter=RequestsWebhookAdapter())
+        webhook.send(embed=embed)
+        return
+
+    if message.content[1:] in emoji_dict:
+        await message.delete()
+        send_emoji(message.content[1:],message) 
+        return
+
+    if message.content.lower() ==  PREFIX+'logout':
+        await client.close()
+
+
+client.run(TOKEN)
