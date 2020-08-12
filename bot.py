0d96d73bc5396b474ab5756befe60e3970cb05a2
Jason Loh <jasonlohcy96@gmail.com>
2020-08-12 22:13:32 +0800

    new emoji

--- bot.py
+++ bot.py
@@ -6,8 +6,8 @@ TOKEN = os.environ['TOKEN']
 PREFIX = '.'
 
 emoji_dict = {
-    'shoot_tnt' : '<a:KaguyaPew_01s:697513110759800843> <a:KaguyaPew_02s:697513110206152744> '\
-                    '<a:KaguyaPew_03s:697513110709469274> <a:KaguyaPew_04s:697513110118072422> <a:TNT_ignite:697513136357376002>',
+    'shoot_tnt' : '<a:KaguyaPew_01s:697513110759800843><a:KaguyaPew_02s:697513110206152744>'\
+                    '<a:KaguyaPew_03s:697513110709469274><a:KaguyaPew_04s:697513110118072422><a:TNT_ignite:697513136357376002>',
     'grass_bounce' : '<a:grass_bounce:587505418406723584>',
     'fast_parrot' : '<a:fast_parrot:393622342581878785>',
     'PepeLazer' : '<a:0PepeLazerRee:701750592271417385>',
@@ -22,10 +22,10 @@ emoji_dict = {
     'monkawash' : '<a:aPES_FranticWash:690958321733337170>',
     'pepebonk' : '<a:aPES4_PaperBonk:733230501455986769>',
     'kittyplay' : '<a:0KittyPlay:700970912656392204>',
-    'sharingan' : '<a:CH_Sharingan1A:730774327007379560> <a:CH_Sharingan1B:730774341339578388> <a:CH_Sharingan1C:730774350789214259> <a:CH_Sharingan1D:730774360704548875> \n'\
-                    '<a:CH_Sharingan2A:730774405231411271> <a:CH_Sharingan2B:730774474340696124> <a:CH_Sharingan2C:730774502895779870> <a:CH_Sharingan2D:730774539335630858>\n' \
-                    '<a:CH_Sharingan3A:730774563192832070> <a:CH_Sharingan3B:730774572252659813> <a:CH_Sharingan3C:730774586035011594> <a:CH_Sharingan3D:730774596667572334>\n' \
-                    '<a:CH_Sharingan4A:730774610571952168> <a:CH_Sharingan4B:730774622169071627> <a:CH_Sharingan4C:730774711088316486> <a:CH_Sharingan4D:730774725688557658>' 
+    'sharingan' : '<a:CH_Sharingan1A:730774327007379560><a:CH_Sharingan1B:730774341339578388><a:CH_Sharingan1C:730774350789214259><a:CH_Sharingan1D:730774360704548875>\n'\
+                    '<a:CH_Sharingan2A:730774405231411271><a:CH_Sharingan2B:730774474340696124><a:CH_Sharingan2C:730774502895779870><a:CH_Sharingan2D:730774539335630858>\n' \
+                    '<a:CH_Sharingan3A:730774563192832070><a:CH_Sharingan3B:730774572252659813><a:CH_Sharingan3C:730774586035011594><a:CH_Sharingan3D:730774596667572334>\n' \
+                    '<a:CH_Sharingan4A:730774610571952168><a:CH_Sharingan4B:730774622169071627><a:CH_Sharingan4C:730774711088316486><a:CH_Sharingan4D:730774725688557658>' 
 
 }
 
